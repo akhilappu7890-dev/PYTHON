73c1098657b4545702b18c5fbf499045feb27e5b
akhilappu7890-dev <akhilappu7890@gmail.com>
2026-01-12 10:27:40 +0530

    Add Account, SavingsAccount, and CurrentAccount classes with interest calculation

---
+++ PYHW/HW9.py
@@ -0,0 +1,34 @@
+class Account:
+    def __init__(self, name, balance):
+        self._name = name         
+        self._balance = balance  
+
+    def __add__(self, other):
+        return self._balance + other._balance
+
+
+class SavingsAccount(Account):
+    def calculate_interest(self):
+        return self._balance * 0.05   
+
+
+class CurrentAccount(Account):
+    def calculate_interest(self):
+        return self._balance * 0.02  
+
+
+savings = SavingsAccount("Raju", 10000)
+current = CurrentAccount("parvathy", 20000)
+
+print("Savings Account")
+print("Name:", savings._name)
+print("Balance:", savings._balance)
+print("Interest:", savings.calculate_interest())
+
+print("\nCurrent Account")
+print("Name:", current._name)
+print("Balance:", current._balance)
+print("Interest:", current.calculate_interest())
+
+total_balance = savings + current
+print("\nTotal Balance (Savings + Current):", total_balance)
